09eade7dbc48622b5ac5528bb519c7aeb27a1ac9
Dr. Martin Lützelberger <webmaster@raspberryblog.de>
2017-10-29 15:27:12 +0100

    Beispiel-Code hinzugefügt

---
+++ basic_blink/basic_blink.ino
@@ -0,0 +1,16 @@
+#define LED_PIN 13
+#define BLINK_TIME 1000
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(LED_PIN, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(LED_PIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(BLINK_TIME);             // wait for a second
+  digitalWrite(LED_PIN, LOW);    // turn the LED off by making the voltage LOW
+  delay(BLINK_TIME);             // wait for a second
+}
