499ea855054b602c4886bdb5a535df42027460fd
Dr. Martin Lützelberger <webmaster@raspberryblog.de>
2017-10-29 15:43:01 +0100

    Beispiel-Code hinzugefügt

---
+++ shift_register2/shift_register2.ino
@@ -0,0 +1,24 @@
+int taktPin = 8;
+int speicherPin = 9;
+int datenPin = 10;
+
+/* Shift Register
+OE (Pin13) => GND
+MR (Pin10) => VCC
+*/
+
+byte wert = 255;
+
+void setup() {
+  pinMode(taktPin, OUTPUT);
+  pinMode(speicherPin, OUTPUT);
+  pinMode(datenPin, OUTPUT);
+
+}
+
+void loop() {
+  digitalWrite(speicherPin, LOW);
+  shiftOut(datenPin, taktPin, MSBFIRST, wert);
+  digitalWrite(speicherPin, HIGH);
+  delay(20);
+}
