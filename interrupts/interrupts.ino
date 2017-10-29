7a0f2d5c3ecbeb652cb3a572dc1756edf753eac0
Dr. Martin Lützelberger <webmaster@raspberryblog.de>
2017-10-29 15:41:36 +0100

    Beispiel-Code hinzugefügt

---
+++ interrupts/interrupts.ino
@@ -0,0 +1,32 @@
+
+const int ledPinBlink = 13; // Rote Blink-LED-Pin 13
+const int ledPinTaster = 10; // Gelbe Taster-LED-Pin 10
+const int tasterPin = 2; // Taster-Pin 8
+bool tasterStatus; // Variable zur Aufname des Tasterstatus
+
+void setup(){
+ pinMode(ledPinBlink, OUTPUT); // Blink-LED-Pin als Ausgang 
+ pinMode(ledPinTaster, OUTPUT); // Taster-LED-Pin als Ausgang
+ pinMode(tasterPin, INPUT_PULLUP); // Taster-Pin als Eingang
+  attachInterrupt(digitalPinToInterrupt(tasterPin), toggle_led, RISING);
+}
+
+void loop(){
+ // Blink-LED blinken lassen
+ digitalWrite(ledPinBlink, HIGH); // Rote LED auf High-Pegel (5V)
+ delay(1000); // Eine Sekunde warten
+ digitalWrite(ledPinBlink, LOW); // Rote LED auf LOW-Pegel (0V)
+ delay(1000); // Eine Sekunde warten
+}
+
+void toggle_led() {  
+  if(tasterStatus == 0) {
+    digitalWrite(ledPinTaster, HIGH); // Gelbe LED auf High-Pegel (5V)
+    tasterStatus = 1;
+  }
+  else 
+  {
+    digitalWrite(ledPinTaster, LOW); // Gelbe LED auf Low-Pegel (0V)
+    tasterStatus = 0;
+   }
+  }
