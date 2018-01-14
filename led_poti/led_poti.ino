52b8fed13c36186542cd0055eb600b73b8380bda
Dr. Martin Lützelberger <m.luetzelberger@telecolumbus.net>
2018-01-14 14:05:59 +0100

    Update Sketches

--- led_poti/led_poti.ino
+++ led_poti/led_poti.ino
@@ -1,22 +1,27 @@
-
 int potPin= A0;
-int LEDPin= 9; 
+int ledPin= 9; 
 int readValue;
 int writeValue;
 
 void setup() {
   pinMode(potPin, INPUT);
-  pinMode(LEDPin, OUTPUT);
+  pinMode(ledPin, OUTPUT);
   Serial.begin(9600);
 }
 
 void loop() {
-  
  readValue = analogRead(potPin);
- writeValue = (255./1023.) * readValue;
- //analogWrite(LEDPin, writeValue);
- analogWrite( LEDPin, map(readValue, 0, 1023, 0, 255) );
- Serial.print("You are writing a value of ");
- Serial.println(writeValue);
- 
+ //writeValue = (255./1023.) * readValue;
+ analogWrite(ledPin, writeValue);
+ analogWrite( ledPin, map(readValue, 0, 1023, 255, 0) );
+ Serial.print("You are reading a value of ");
+ Serial.println(readValue);
+ //if (readValue <= 500) {
+ //   analogWrite( ledPin, 255);
+ // }
+ // else {
+ // analogWrite ( ledPin, 0);
+ // } 
+ //Serial.print("You are writing a value of ");
+ //Serial.println(writeValue);
 }
